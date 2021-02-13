47535a64216d1f293b669539b3c16bae6f66d48a
pascal7979 <72535419+pascal7979@users.noreply.github.com>
2021-02-13 17:31:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
-git clone https://github.com/kenzok8/small.git  package/openwrt-small
+# git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git  package/openwrt-small
 # Lienol/openwrt-package
 sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
